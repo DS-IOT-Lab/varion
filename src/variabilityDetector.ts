c286328aa292871049a056e6ad2b30af45be6748
navid <navid9675@gmail.com>
2018-02-26 19:26:48 +0330

    + git-ignore file corrected (I guess!)

--- src/variabilityDetector.ts
+++ src/variabilityDetector.ts
@@ -1,15 +1,32 @@
 /**
  * Created by navid on 2/23/18.
  */
+
 import {readFileSync} from "fs";
 import * as ts from "typescript";
+
+// TODO: find a proper name for variation point identifier instead of "variation"
 
 export function checkVariability(sourceFile: ts.SourceFile) {
     checkVariabilityForNode(sourceFile);
 
     // searches node comments and JSDocs for any variability identifiers
     function checkVariabilityForNode(node: ts.Node) {
-        // TODO: search!
+        console.log("<========>");
+        console.log(node.kind + " pos: " + node.pos + " end:" + node.end);
+
+        let comments = ts.getLeadingCommentRanges(sourceFile.text, node.pos);
+        console.log(comments);
+
+        if (node.kind == ts.SyntaxKind.Block) {
+            console.log("Block Found!");
+
+            // TODO: extract the "@variation" from the comment using the comment.end and comment.start
+            let x = sourceFile.text;
+        }
+        console.log("<======>\n");
+
+        ts.forEachChild(node, checkVariabilityForNode);
     }
 
 }
