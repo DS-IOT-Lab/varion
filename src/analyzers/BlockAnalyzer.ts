e5499dd4f771ed8c7dbc7908c367d455a679c332
navid <navid9675@gmail.com>
2018-04-19 18:43:04 +0430

    + Refactored + Finding Variation points inside method's body + extract variation condition from single line comments and print them
    # TODO: parse and evaluate single line comments by doctrine modules.

---
+++ src/analyzers/BlockAnalyzer.ts
@@ -0,0 +1,105 @@
+import {
+    ImportDeclaration,
+    SourceFile,
+    SyntaxKind,
+    Block,
+    ClassDeclaration,
+    ts,
+    JSDoc,
+    JSDocTag,
+    Decorator,
+    MethodDeclaration,
+    Node, IfStatement,
+} from 'ts-simple-ast';
+import { CommentRange } from 'typescript';
+import { ConditionEvaluator } from '../ConditionEvaluator';
+import { Analyzer } from './Analyzer';
+import { JSDocAnalyzer } from './JSDocAnalyzer';
+import { doctrine } from 'doctrine';
+import {isUndefined} from "util";
+
+const BLOCK_CONTAINING_NODES: Array<SyntaxKind> = 
+[SyntaxKind.IfStatement, 
+    SyntaxKind.WhileStatement, 
+    SyntaxKind.DoStatement, 
+    SyntaxKind.Block, 
+    SyntaxKind.ForStatement];
+
+export class BlockAnalyzer {
+
+    public analyze(sourceFile: SourceFile, node: Block) {
+        this.analyzeBlock(sourceFile, node);
+        return false;
+    }
+    
+    public traverseBlockContent(sourceFile: SourceFile, node: Node) {
+        if(BLOCK_CONTAINING_NODES.indexOf(node.getKind()) != -1) {
+            
+            switch (node.getKind()) {
+                case SyntaxKind.Block: 
+                    this.analyzeBlock(sourceFile, node as Block);
+                    break;
+                    
+                case SyntaxKind.IfStatement:
+                    this.analyzeIfStatements(sourceFile, node as IfStatement);
+                    break;
+                case SyntaxKind.DoStatement: break;
+                case SyntaxKind.WhileStatement: break;
+                case SyntaxKind.ForStatement: break;
+            }
+        }
+        
+        return false; // FIXME: fix here!
+    }
+    
+    private analyzeBlock(sourceFile: SourceFile, block: Block) {
+        // first check the block it self!
+        let isIncluded = this.analyzeLeadingComments(sourceFile, block);
+
+        // if the block is not excluded then check its body
+        if(isIncluded) {
+            let blockContents: Node[] = block.getChildren()[1].getChildren();    // getting 'SyntaxList' children
+
+            for(let i=0; i<blockContents.length; i++) {
+                this.traverseBlockContent(sourceFile, blockContents[i]);
+            }
+        }
+    }
+
+    private analyzeIfStatements(sourceFile: SourceFile, ifStatement: IfStatement){
+        let blockContents: Block[] = ifStatement.getChildrenOfKind(SyntaxKind.Block);
+        let elifContents: IfStatement[] = ifStatement.getChildrenOfKind(SyntaxKind.IfStatement);
+
+        for(let i=0; i<blockContents.length; i++) {
+            this.traverseBlockContent(sourceFile, blockContents[i]);
+        }
+
+        for(let i=0; i<elifContents.length; i++) {
+            this.analyzeIfStatements(sourceFile, elifContents[i]);
+        }
+    }
+
+    private analyzeLeadingComments(sourceFile: SourceFile, block: Block): boolean {
+        let commentsRange: CommentRange[] = ts.getLeadingCommentRanges(sourceFile.getText(), block.getPos());
+
+        if(isUndefined(commentsRange)) {
+            return true;
+        }
+
+        for (let i = 0; i < commentsRange.length; i++) {
+            this.parseComment(sourceFile, block, commentsRange[i]);
+            // TODO: evaluate comment and apply variability if required
+        }
+
+        return true;
+    }
+
+    private parseComment(sourceFile: SourceFile,
+        block: Block,
+        commentRange: CommentRange) {
+
+        let commentText: string = sourceFile.getText().slice(commentRange.pos, commentRange.end);
+
+        console.log('Extracted Comment: ' + commentText); // TODO: parse and check
+    }
+}
---
+++ src/analyzers/MethodAnalyzer.ts
@@ -0,0 +1,115 @@
+import {
+    ImportDeclaration,
+    SourceFile,
+    SyntaxKind,
+    Block,
+    ClassDeclaration,
+    ts,
+    JSDoc,
+    JSDocTag,
+    Decorator,
+    MethodDeclaration,
+    Node
+} from "ts-simple-ast";
+import { ConditionEvaluator } from "../ConditionEvaluator";
+import { Analyzer } from './Analyzer';
+import { JSDocAnalyzer } from './JSDocAnalyzer';
+import { BlockAnalyzer } from './BlockAnalyzer';
+
+// singleton 
+export class MethodAnalyzer extends Analyzer {
+    private static instance: MethodAnalyzer;
+
+
+    private constructor() {
+        super();
+    }
+
+    public static getInstance() {
+        if (MethodAnalyzer.instance == null) {
+            MethodAnalyzer.instance = new MethodAnalyzer();
+        }
+
+        return MethodAnalyzer.instance;
+    }
+
+    public analyze(sourceFile: SourceFile,
+        node: Node<ts.MethodDeclaration>): boolean {
+        let methodDeclaration: MethodDeclaration = node as MethodDeclaration;
+        console.log('Analyzing Method ' + methodDeclaration.getName() + ' ...');
+
+        let jsDocs: JSDoc[] = methodDeclaration.getJsDocs();
+        let isIncluded: boolean = true;
+
+        // inspecting each method's JS-Doc
+        for (let i = 0; i < jsDocs.length; i++) {
+            isIncluded = JSDocAnalyzer.analyze(jsDocs[i]);
+            if (!isIncluded) {
+                this.removeMethod(sourceFile, methodDeclaration);
+                sourceFile.emit();
+                return false;
+            }
+        }
+
+        if (isIncluded) {
+            this.checkMethodBody(methodDeclaration);
+            return true;
+        }
+    }
+
+    private removeMethod(sourceFile: SourceFile,
+        methodDeclaration: MethodDeclaration) {
+
+        console.log('Removing method initiation sequence: (#Method, '
+            + methodDeclaration.getName()
+            + ', '
+            + methodDeclaration.getPos()
+            + ', '
+            + methodDeclaration.getEnd()
+            + ')');
+
+        // getting and removing method decorators
+        let methodDecorators: Decorator[] = methodDeclaration.getDecorators();
+        console.log('1) Removing ' + methodDeclaration.getName() + ' decorators:');
+        for (let i = 0; i < methodDecorators.length; i++) {
+            console.log('1- ' + i + ') Removing decorator: (#Decorator, '
+                + methodDecorators[i].getName()
+                + ', '
+                + methodDecorators[i].getPos()
+                + ', '
+                + methodDecorators[i].getEnd()
+                + ')');
+
+            methodDecorators[i].remove();
+        }
+
+        // getting and removing method JSDocs
+        console.log('2) Removing ' + methodDeclaration.getName() + ' JS Docs:');
+        let jsDocs: JSDoc[] = methodDeclaration.getJsDocs();
+        for (let i = 0; i < jsDocs.length; i++) {
+            console.log('2-' + i + ') Removing JSDoc: (#JSDoc, '
+                + jsDocs[i].getPos()
+                + ', '
+                + jsDocs[i].getEnd()
+                + ')');
+
+            jsDocs[i].remove();
+        }
+
+        // removing method itself
+        console.log('3) Removing method: ' + methodDeclaration.getName());
+        methodDeclaration.remove();
+    }
+
+    private checkMethodBody(methodDeclaration: MethodDeclaration) {
+        if(methodDeclaration.hasBody()) {
+            let block = methodDeclaration.getChildrenOfKind(SyntaxKind.Block)[0];
+            let blkAnalyzer = new BlockAnalyzer();
+
+            blkAnalyzer.analyze(methodDeclaration.getSourceFile(), block as Block);
+        }
+    }
+
+
+
+}
