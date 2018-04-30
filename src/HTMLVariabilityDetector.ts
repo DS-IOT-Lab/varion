2e12cbf77512e287be36fbe01e72abf4845910e3
navid <navid9675@gmail.com>
2018-04-30 19:50:14 +0430

    + Applied variability on html files. # TODO: search for the tags that contains 'condition' attribute and apply variability on them. # TODO: write variability applied html sources to files.

--- src/HTMLVariabilityDetector.ts
+++ src/HTMLVariabilityDetector.ts
@@ -1,4 +1,4 @@
-import * as $ from 'cheerio';
+import * as cheerio from 'cheerio';
 import {
     ImportDeclaration,
     SourceFile,
@@ -13,11 +13,41 @@ import {
     Node
 } from "ts-simple-ast";
 import {ConditionEvaluator} from './ConditionEvaluator';
-import {HTMLAnalyzer} from './htmlAnalyzer/HTMLAnalyzer'; 
+import {HTMLAnalyzer} from './htmlAnalyzer/HTMLAnalyzer';
+import * as fs from "fs";
+import chalk from "chalk";
+import {isUndefined} from "util";
 
 
 export class HTMLVariabilityDetector {
-    public analyzeSourceFile(sourceFile: SourceFile) {
-        
+    public static analyzeSourceFile(sourceText: string, file) {
+        let $ = cheerio.load(sourceText);
+        let presenceTags = $('presence');
+
+        console.log(chalk.cyan(file));
+        for (let i = 0; i < presenceTags.length; i++) {
+            let conditionExp = $(presenceTags[i]).attr('condition');
+
+            if (conditionExp != undefined) {
+                let res = ConditionEvaluator.evaluate(conditionExp);
+                console.log(chalk.green('\tCondition Expression: ') + conditionExp + chalk.blue(', Result -> ') + res);
+
+                if (!res) {
+                    $(presenceTags[i]).empty();
+                    $(presenceTags[i]).remove();
+                } else {
+                    let content = $(presenceTags[i]).contents();
+                    $(presenceTags[i]).replaceWith(content);
+                }
+
+            } else {
+                console.log(chalk.green('\tCondition Expression: ') + 'No Condition Expression Found!');
+            }
+        }
+
+        if (presenceTags.length == 0) {
+            console.log('\tNo variation point found for this document.')
+        }
+        return $.html()
     }
 }
--- src/VariabilityManager.ts
+++ src/VariabilityManager.ts
@@ -3,10 +3,17 @@
  */
 
 import Project, { SourceFile } from "ts-simple-ast";
+import * as glob from 'glob';
+import * as fs from 'fs';
 import { TypeScriptVariabilityDetector } from "./TypeScriptVariabilityDetector";
+import {HTMLVariabilityDetector} from "./HTMLVariabilityDetector";
 import { ConditionEvaluator } from "./ConditionEvaluator";
+import chalk from "chalk";
 
+
+let htmlDerivedFiles: Array<HtmlSource> = [];
 var jsonConfig;
+let that;
 
 export class VariabilityManager {
     private rootDirectoryPath: String;
@@ -22,33 +29,78 @@ export class VariabilityManager {
         this.targetDirectoryPath = targetDirectoryPath;
         this.configurationPath = configurationPath;
         this.rootDirectoryPath = rootDirectoryPath;
-        console.log('@@@@@@@@@@@');
         this.project = new Project({ compilerOptions: { outDir: targetDirectoryPath.toString() } });
         this.project.addExistingSourceFiles(rootDirectoryPath + "/**/*.ts");
-        console.log('@@@@@@@@@@@');
 
-        this.init();
+        this.startAnalyzingTypeScripts();
+        this.startAnalyzingHTMLFiles();
     }
 
-    private init() {
+    private startAnalyzingTypeScripts() {
         let sourceFiles = this.project.getSourceFiles();
 
         for (let srcIndex in sourceFiles) {
             let srcFile: SourceFile = sourceFiles[srcIndex];
             console.log("Analyzing " + srcFile.getBaseName());
-            console.log(srcFile.getExtension());
-            
+
             if (srcFile.getExtension() == '.ts') {
                 TypeScriptVariabilityDetector.analyzeSourceFile(srcFile);
-                
-            } else if (srcFile.getExtension() == '.html') {
-                srcFile.removeText(0, srcFile.getEnd());
             }
         }
-        
+
         const srcDir = this.project.getDirectoryOrThrow(this.rootDirectoryPath.toString());
         // need to specify false here to prevent it from including other files in the directory
         const newDir = srcDir.copy(this.targetDirectoryPath.toString(), { includeUntrackedFiles: false });
         newDir.save();
+    }
+
+    private startAnalyzingHTMLFiles() {
+        that = this;
+        glob(this.rootDirectoryPath + '/**/*.html', this.htmlCallback);
+    }
+
+    private htmlCallback(error, files) {
+
+        for (let i = 0; i < files.length; i++) {
+            let sourceFileText = fs.readFileSync(files[i], 'utf8');
+            let derivedSource = HTMLVariabilityDetector.analyzeSourceFile(sourceFileText, files[i]);
+
+            htmlDerivedFiles.push(new HtmlSource(files[i], derivedSource));
+        }
+
+        that.printHtmlFiles();
+    }
+
+    private printHtmlFiles() {
+        for (let i = 0; i < htmlDerivedFiles.length; i++) {
+            console.log(chalk.cyan('HTML file path: ') + htmlDerivedFiles[i].getSourcePath());
+            console.log(chalk.gray(htmlDerivedFiles[i].getSourceText()));
+        }
+    }
+}
+
+class HtmlSource {
+    private sourcePath: string;
+    private sourceText: string;
+
+    constructor(sourcePath: string, sourceText: string) {
+        this.sourcePath = sourcePath;
+        this.sourceText = sourceText;
+    }
+
+    public getSourcePath() {
+        return this.sourcePath;
+    }
+
+    public getSourceText() {
+        return this.sourceText;
+    }
+
+    public setSourceText(sourceText: string) {
+        this.sourceText = sourceText;
+    }
+
+    public setSourcePath(sourcePath: string) {
+        this.sourcePath = sourcePath;
     }
 }
