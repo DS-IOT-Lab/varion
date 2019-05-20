505592b08a457162bfdc6e4d1984c1546037310e
Navid Alipour <navid9675@gmail.com>
2019-05-20 15:34:41 +0430

    Fix method name typo and reorganize the methods

--- src/helper/variationContainer/AbstractVariationPointContainer.ts
+++ src/helper/variationContainer/AbstractVariationPointContainer.ts
@@ -3,20 +3,20 @@ import {VariationPointContainerType} from "../VariationPointContainerType";
 export interface AbstractVariationPointContainer {
     getInternalVariationPoints(): Array<AbstractVariationPointContainer>;
 
+    addToInternalVariationPoints(variationPoint: AbstractVariationPointContainer);
+
     getVariationExpression(): String;
 
     variationExpressionContains(conditionExpression: String): boolean;
-
-    printInfo(): String;
-
-    applyVariation();
 
     setVariationPointState(status: Boolean);
 
     getVariationPointState(): Boolean;
 
-    addToInternalVariationPoint(variationPoint: AbstractVariationPointContainer);
-
     getVariationPointType(): VariationPointContainerType;
+
+    applyVariation();
+
+    printInfo(): String;
 
 }
