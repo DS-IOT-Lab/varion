5805e2661ce1ef8a4e7abbfc0054fe921e0ccb5d
Navid Alipour <navid9675@gmail.com>
2019-05-24 18:13:52 +0430

    Refactor Block, Method, Class analyzers
    Refactor `BlockAnalyzer`, `ClassAnalyzer`, `MethodAnalyzer` classes.
    Refactor includes:
    - Moving most of the variability application process to the variation point container classes
    - Using `VariationPoint` container classes to store variation points after analysis over source codes
    - Using `VariationPointStatus` enumeration to show the state of a variation point defined in the source code based on the configuration file
    - Code style and formatting
    
    **NOTE:** Previously analyzer classes were also responsible to apply the variabilities but now they are only responsible to extract the variation points defined in the source code.

--- src/main/typeScriptAnalyzers/MethodAnalyzer.ts
+++ src/main/typeScriptAnalyzers/MethodAnalyzer.ts
@@ -1,21 +1,11 @@
-import {
-    ImportDeclaration,
-    SourceFile,
-    SyntaxKind,
-    Block,
-    ClassDeclaration,
-    ts,
-    JSDoc,
-    JSDocTag,
-    Decorator,
-    MethodDeclaration,
-    Node
-} from "ts-simple-ast";
+import {Block, JSDoc, MethodDeclaration, Node, SourceFile, SyntaxKind, ts} from "ts-simple-ast";
 
-import { ConditionEvaluator } from "../ConditionEvaluator";
-import { Analyzer } from './Analyzer';
-import { DocCommentAnalyzer } from './DocCommentAnalyzer';
-import { BlockAnalyzer } from './BlockAnalyzer';
+import {Analyzer} from './Analyzer';
+import {DocCommentAnalyzer} from './DocCommentAnalyzer';
+import {BlockAnalyzer} from './BlockAnalyzer';
+import {AbstractVariationPointContainer} from "../../helper/variationContainer/AbstractVariationPointContainer";
+import {BlockVariationPoint} from "../../helper/variationContainer/typescript/implementation/BlockVariationPoint";
+import {VariationPointStatus} from "../../helper/VariationPointStatus";
 
 /**
  * This Class checks the variability defined for each method.
@@ -45,98 +35,46 @@ export class MethodAnalyzer extends Analyzer {
      * This method handles variation points set for a method inside a class.
      * @param  sourceFile [description]
      * @param  node       [description]
+     * @param containerVariationPoint
      * @return            [description]
      */
-    public analyze(sourceFile: SourceFile,
-        node: Node<ts.MethodDeclaration>): boolean {
+    public analyze(sourceFile: SourceFile
+        , node: Node<ts.MethodDeclaration>
+        , containerVariationPoint: AbstractVariationPointContainer) {
         try {
             let methodDeclaration: MethodDeclaration = node as MethodDeclaration;
-            console.log('\tAnalyzing Method ' + methodDeclaration.getName() + ' ...');
 
             let jsDocs: JSDoc[] = methodDeclaration.getJsDocs();
-            let isIncluded: boolean = true;
+            let variationRes: VariationPointStatus = VariationPointStatus.UNDEFINED;
 
             // inspecting each method's JS-Doc
             for (let i = 0; i < jsDocs.length; i++) {
-                isIncluded = DocCommentAnalyzer.analyzeJsDoc(jsDocs[i]);
-                if (!isIncluded) {
-                    this.removeMethod(sourceFile, methodDeclaration);
-                    sourceFile.emit();
-                    return false;
-                }
+                variationRes = DocCommentAnalyzer.analyzeJsDoc(jsDocs[i]);
+                containerVariationPoint.setVariationPointState(variationRes);
+                // if (variationRes == VariationPointStatus.NOT_INCLUDED) {
+                //     containerVariationPoint.setVariationPointState(VariationPointStatus.NOT_INCLUDED);
+                // }
             }
+            this.checkMethodBody(methodDeclaration, containerVariationPoint);
 
-            // check if method is included
-            if (isIncluded) {
-                this.checkMethodBody(methodDeclaration);
-                return true;
-            }
         } catch (e) {
             console.log(e.toString());
-            return true;
-
         }
     }
 
-    /**
-     * This method removes the given method from the sourceFile.
-     * @param  sourceFile        {SourceFile} source file which contains the method
-     * @param  methodDeclaration {MethodDeclaration} methodDeclaration node
-     */
-    private removeMethod(sourceFile: SourceFile,
-        methodDeclaration: MethodDeclaration) {
-
-        console.log('Removing method initiation sequence: (#Method, '
-            + methodDeclaration.getName()
-            + ', '
-            + methodDeclaration.getPos()
-            + ', '
-            + methodDeclaration.getEnd()
-            + ')');
-
-        // getting and removing method decorators
-        let methodDecorators: Decorator[] = methodDeclaration.getDecorators();
-        console.log('1) Removing ' + methodDeclaration.getName() + ' decorators:');
-        for (let i = 0; i < methodDecorators.length; i++) {
-            console.log('1- ' + i + ') Removing decorator: (#Decorator, '
-                + methodDecorators[i].getName()
-                + ', '
-                + methodDecorators[i].getPos()
-                + ', '
-                + methodDecorators[i].getEnd()
-                + ')');
-
-            methodDecorators[i].remove();
-        }
-
-        // getting and removing method JSDocs
-        console.log('2) Removing ' + methodDeclaration.getName() + ' JS Docs:');
-        let jsDocs: JSDoc[] = methodDeclaration.getJsDocs();
-        for (let i = 0; i < jsDocs.length; i++) {
-            console.log('2-' + i + ') Removing JSDoc: (#JSDoc, '
-                + jsDocs[i].getPos()
-                + ', '
-                + jsDocs[i].getEnd()
-                + ')');
-
-            jsDocs[i].remove();
-        }
-
-        // removing method itself
-        console.log('3) Removing method: ' + methodDeclaration.getName());
-        methodDeclaration.remove();
-    }
 
     /**
      * This method searches for variation points defined inside a method
      * @param  methodDeclaration {MethodDeclaration} given mehtod declaration to search inside
      */
-    private checkMethodBody(methodDeclaration: MethodDeclaration) {
-        if(methodDeclaration.hasBody()) {
+    private checkMethodBody(methodDeclaration: MethodDeclaration, containerVariationPoint: AbstractVariationPointContainer) {
+        if (methodDeclaration.hasBody()) {
             let block = methodDeclaration.getChildrenOfKind(SyntaxKind.Block)[0];
             let blkAnalyzer = new BlockAnalyzer();
+            let blockVariationPoint = new BlockVariationPoint(methodDeclaration.getSourceFile(), block);
 
-            blkAnalyzer.analyze(methodDeclaration.getSourceFile(), block as Block);
+            containerVariationPoint.addToInternalVariationPoints(blockVariationPoint);
+            blkAnalyzer.analyze(methodDeclaration.getSourceFile(), block as Block, blockVariationPoint);
         }
     }
 }
